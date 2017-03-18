daaa9c2adb03bb4169f9c75993757009d1f4ee98
Cosin <cosin@sohu.com>
2017-03-18 22:27:17 +0800

    base code for namespaced yii

---
+++ site_demo/entry/dev/config/config-DEV.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+    'name' => 'Yii-Namespaced Dev Site [DEV]',
+    'modules' => array(
+        'gii'=>array(
+            'class'=>'system.gii.GiiModule',
+            'password'=>'root',
+            'ipFilters'=>array('127.0.0.1','::1'),
+        ),
+    ),
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'mysql:host=127.0.0.1;dbname=demo',
+            'emulatePrepare' => FALSE,
+            'username' => 'root',
+            'password' => 'root',
+            'charset'  => 'utf8',
+        ),
+    ),
+);
